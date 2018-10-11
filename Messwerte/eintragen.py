a4745c5226ef6a32b526ac7c3ed245a2a4ea80f3
Kevin Mika <kevin.mika@tu-dortmund.de>
2018-10-11 18:15:09 +0200

    123

--- Messwerte/eintragen.py
+++
@@ -1,8 +0,0 @@
-x = input( "in welche Messdatei sollen die Daten rein ?" )
-kl=open( "messwerte" +x+ ".csv" , "w+" )
-while True:
-    n = input()
-    if n == "exit" :
-        break
-    else:
-        kl.write(n+ "," + "\n" )
